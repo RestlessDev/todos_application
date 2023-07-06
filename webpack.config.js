4635171e3ecb7ab3e9282549eee328cb1388c396
Mike Arace <mike@restlessdev.com>
2023-07-06 14:38:18 -0700

    daily updates

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const ErstwhileCompiler = require('./framework/static/ErstwhileCompiler')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './build/bootstrap.js',
@@ -48,10 +47,6 @@ module.exports = {
     new webpack.ProvidePlugin({
       process: 'process/browser',
     }),
-    new CleanWebpackPlugin( {
-        cleanStaleWebpackAssets: true,
-        protectWebpackAssets: true
-      }),
     // new HtmlWebpackPlugin(),
     /*
     {
