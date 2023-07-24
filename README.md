dfdddfbf2ee68c7f2182540805843f35ec88da1c
Mike Arace <mike@restlessdev.com>
2023-07-24 13:47:37 -0700

    daily updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Todos Sample Application
 This repository is for the Todos application, meant as a demonstration for the Erstwhile Framework.
+
+Instructions for setting it up can be found at https://erstwhilefw.org
--- themes/mazer/TextareaInput/component.js
+++ themes/mazer/TextareaInput/component.js
@@ -3,13 +3,13 @@ const ErstwhileControl = require('../../../framework/static/components/Erstwhile
 const ejs = require('ejs');
 const jquery = require("jquery")
 
-class TextInput extends ErstwhileControl {
+class TextareaInput extends ErstwhileControl {
   getValue() {
     let retval = {};
     retval[this.args.name ? this.args.name : this.id] = jquery(`#${this.id}-field`).val();
     return retval;
   }
-
+  
 }
 
-module.exports = TextInput;
+module.exports = TextareaInput;
